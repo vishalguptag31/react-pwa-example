690097b62786e607d7fd5231fd4463213114dec4
vishalguptag31 <vishalguptag31@gmail.com>
2020-08-02 02:02:45 +0530

    added offline mode of service-worker

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,26 @@
 
-console.warn('service worker from public folder');
+let cacheData = "appCache";
+
+this.addEventListener('install' , (event)=>{
+    event.waitUntil(
+        caches.open(cacheData).then((cache)=>{
+            cache.addAll([
+                "/static/js/bundle.js",
+                "/static/js/main.chunk.js",
+                "/static/js/0.chunk.js",
+                "index.html",
+                "/"
+            ])
+        })
+    )
+})
+
+this.addEventListener("fetch",(event)=>{
+    event.respondWith(
+        caches.match(event.request).then((result)=>{
+            if(result){
+                return result;
+            }
+        })
+    )
+})
